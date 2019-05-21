9c7b836abe7db7e3882b8c4ee51c4cab9b821d99
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-05-21 10:46:16 +0300

    Fix SSRProvider export

--- src/css.tsx
+++ src/css.tsx
@@ -2,6 +2,8 @@ import Stylis from "stylis";
 
 import { createCSSTag } from "./css-core";
 
+export { SSRProvider } from "./css-core";
+
 const customStylis = new Stylis({
     prefix: process.env.NODE_ENV === "production",
 });
