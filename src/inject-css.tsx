d0e369075892ba24bd025bf13b1d734fb9fe6e03
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-05-22 10:18:55 +0300

    Fix style injection with hot reload

--- src/inject-css.tsx
+++ src/inject-css.tsx
@@ -4,7 +4,53 @@ import { StyleSheet } from "@emotion/sheet";
 let sheet: StyleSheet | null = null;
 
 if (isBrowser()) {
-    sheet = new StyleSheet({ key: "bemed", container: document.head });
+    sheet = new StyleSheet({
+        key: "bemed",
+        container: document.head,
+        speedy: true,
+    });
+}
+
+/**
+ * Use fast style injection production provided by @emotion/sheet
+ */
+function productionInject(id: string, css: string) {
+    if (sheet) {
+        sheet.insert(css);
+    }
+}
+
+const DEV_STYLE_TAGS: Record<
+    string,
+    { el: HTMLStyleElement; css: string } | undefined
+> = {};
+
+/**
+ * In development use slower injection method that allows source maps and hot
+ * reload
+ */
+function devInject(id: string, css: string) {
+    const existing = DEV_STYLE_TAGS[id];
+
+    if (existing) {
+        if (existing.el.innerHTML !== css) {
+            console.log("react-bemed: hot update: " + id);
+            existing.el.innerHTML = css;
+            existing.el.dataset.ver = String(
+                Number(existing.el.dataset.ver) + 1,
+            );
+        }
+    } else {
+        const style = document.createElement("style");
+        style.dataset.bemed = id;
+        style.dataset.ver = "1";
+        style.innerHTML = css;
+        document.head.appendChild(style);
+        DEV_STYLE_TAGS[id] = {
+            el: style,
+            css,
+        };
+    }
 }
 
 export function injectGlobal(id: string, css: string, sourceMap: string) {
@@ -12,7 +58,9 @@ export function injectGlobal(id: string, css: string, sourceMap: string) {
         return;
     }
 
-    if (sheet) {
-        sheet.insert(css + sourceMap);
+    if (process.env.NODE_ENV !== "production") {
+        return devInject(id, css + sourceMap);
     }
+
+    return productionInject(id, css);
 }
