87aedbdaf0b0ddcabd9b4c1e768696404e4c61a7
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-05-26 20:07:42 +0300

    Export stuff from css-precompiled too

--- src/css-precompiled.tsx
+++ src/css-precompiled.tsx
@@ -1,5 +1,7 @@
 import { createCSSTag } from "./css-core";
 
+export { SSRProvider, createClassName } from "./css-core";
+
 function replaceCompiler(selector: string, cssString: string) {
     return cssString.replace(/__BEMED__/g, selector);
 }
