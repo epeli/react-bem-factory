80dd06b2937b6ba84e735b25610f19aaab90ca4f
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-05-30 20:58:39 +0300

    Consume the rule delimeter in dev too

--- src/inject-css.tsx
+++ src/inject-css.tsx
@@ -34,7 +34,10 @@ const DEV_STYLE_TAGS: Record<
  * reload
  */
 function devInject(id: string, css: string) {
-    css = css.trim();
+    css = css
+        .trim()
+        .split(DELIMETER)
+        .join("\n");
     const existing = DEV_STYLE_TAGS[id];
 
     if (existing) {
