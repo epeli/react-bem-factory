c2dc8b63e9c501396e05933aab0128b17b386397
Esa-Matti Suuronen <esa-matti@suuronen.org>
2020-12-03 20:41:05 +0200

    Use murmur hash

--- src/css-core.tsx
+++ src/css-core.tsx
@@ -6,26 +6,68 @@ declare const process: any;
 
 type StyleRenderRecord = Record<string, true | number>;
 
-const SEED = 5381;
+/**
+ * From https://github.com/emotion-js/emotion/blob/0d47e57d143f96efd2846a1cac9e8bb29c0650fb/packages/hash/src/index.js
+ */
+function hash(str: string) {
+    // 'm' and 'r' are mixing constants generated offline.
+    // They're not really 'magic', they just happen to work well.
 
-// When we have separate strings it's useful to run a progressive
-// version of djb2 where we pretend that we're still looping over
-// the same string
-const phash = (h: number, x: string) => {
-    let i = x.length;
+    // const m = 0x5bd1e995;
+    // const r = 24;
 
-    while (i) {
-        h = (h * 33) ^ x.charCodeAt(--i);
+    // Initialize the hash
+
+    var h = 0;
+
+    // Mix 4 bytes at a time into the hash
+
+    var k,
+        i = 0,
+        len = str.length;
+    for (; len >= 4; ++i, len -= 4) {
+        k =
+            (str.charCodeAt(i) & 0xff) |
+            ((str.charCodeAt(++i) & 0xff) << 8) |
+            ((str.charCodeAt(++i) & 0xff) << 16) |
+            ((str.charCodeAt(++i) & 0xff) << 24);
+
+        k =
+            /* Math.imul(k, m): */
+            (k & 0xffff) * 0x5bd1e995 + (((k >>> 16) * 0xe995) << 16);
+        k ^= /* k >>> r: */ k >>> 24;
+
+        h =
+            /* Math.imul(k, m): */
+            ((k & 0xffff) * 0x5bd1e995 + (((k >>> 16) * 0xe995) << 16)) ^
+            /* Math.imul(h, m): */
+            ((h & 0xffff) * 0x5bd1e995 + (((h >>> 16) * 0xe995) << 16));
     }
 
-    return h;
-};
+    // Handle the last few bytes of the input array
 
-// This is a djb2 hashing function. Borrowed from styled-components (compat
-// license)
-const hash = (x: string) => {
-    return String(phash(SEED, x));
-};
+    switch (len) {
+        case 3:
+            h ^= (str.charCodeAt(i + 2) & 0xff) << 16;
+        case 2:
+            h ^= (str.charCodeAt(i + 1) & 0xff) << 8;
+        case 1:
+            h ^= str.charCodeAt(i) & 0xff;
+            h =
+                /* Math.imul(h, m): */
+                (h & 0xffff) * 0x5bd1e995 + (((h >>> 16) * 0xe995) << 16);
+    }
+
+    // Do a few final mixes of the hash to ensure the last few
+    // bytes are well-incorporated.
+
+    h ^= h >>> 13;
+    h =
+        /* Math.imul(h, m): */
+        (h & 0xffff) * 0x5bd1e995 + (((h >>> 16) * 0xe995) << 16);
+
+    return ((h ^ (h >>> 15)) >>> 0).toString(36);
+}
 
 /**
  * Record CSS strings that are rendered to DOM
